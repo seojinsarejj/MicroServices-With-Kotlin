e999027eedf9d8ff62e403fed24163d93b7206e6
seojinsarejj <s19009@naver.com>
2021-11-25 20:04:18 +0900

    Create First Controller

--- src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -2,9 +2,20 @@ package com.microservices.chapter2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
 
 @SpringBootApplication
 class Chapter2Application
+
+@Controller
+class FirstController {
+	@RequestMapping(value = ["/user"], method = arrayOf(RequestMethod.GET))
+	@ResponseBody
+	fun hello() = "hello world"
+}
 
 fun main(args: Array<String>) {
 	runApplication<Chapter2Application>(*args)

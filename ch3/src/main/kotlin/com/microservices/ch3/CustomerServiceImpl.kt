65f053022ac7dffcc9e6b2de6fec38ba88f63684
seojinsarejj <s19009@naver.com>
2021-11-29 15:21:46 +0900

    Create implementing customer service interface service

---
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerServiceImpl.kt
@@ -0,0 +1,23 @@
+package com.microservices.ch3
+
+class CustomerServiceImpl : CustomerService {
+    override fun getCustomer(id: Int): Customer? {
+        TODO("Not yet implemented")
+    }
+
+    override fun createCustomer(customer: Customer) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteCustomer(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateCustomer(id: Int, customer: Customer) {
+        TODO("Not yet implemented")
+    }
+
+    override fun searchCustomer(nameFilter: String): List<Customer> {
+        TODO("Not yet implemented")
+    }
+}

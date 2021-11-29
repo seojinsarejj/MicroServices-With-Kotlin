2a5aae0cfe05b14ea82dee3cb649fac02cf6d633
seojinsarejj <s19009@naver.com>
2021-11-29 14:44:24 +0900

    Create Customer Service Interface

---
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerService.kt
@@ -0,0 +1,9 @@
+package com.microservices.ch3
+
+interface CustomerService {
+    fun getCustomer(id: Int) : Customer?
+    fun createCustomer(customer: Customer)
+    fun deleteCustomer(id: Int)
+    fun updateCustomer(id: Int, customer: Customer)
+    fun searchCustomer(nameFilter: String): List<Customer>
+}

337715da3ebd154e09d1ccc0c0bc4655f70faf5a
seojinsarejj <s19009@naver.com>
2021-11-29 13:50:39 +0900

    Add path variable on customer controller

--- ch3/src/main/kotlin/com/microservices/ch3/Ch3Application.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/Ch3Application.kt
@@ -2,9 +2,20 @@ package com.microservices.ch3
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class Ch3Application
+class Ch3Application {
+	companion object {
+		val initialCutomers = arrayOf(Customer(1, "Kotlin"),
+			Customer(2,"Spring"),
+			Customer(3,"Microservice"))
+	}
+
+	@Bean
+	fun customer() = ConcurrentHashMap<Int,Customer>(initialCutomers.associateBy(Customer::id))
+}
 
 fun main(args: Array<String>) {
 	runApplication<Ch3Application>(*args)
--- ch3/src/main/kotlin/com/microservices/ch3/CustomerController.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerController.kt
@@ -1,12 +1,14 @@
 package com.microservices.ch3
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController {
-    @RequestMapping(value = ["/customer"], method = arrayOf(RequestMethod.GET))
-    fun getCustomer() = Customer(1, "Kotlin")
+    @Autowired
+    lateinit var customers : ConcurrentHashMap<Int, Customer>
+
+    @RequestMapping(value = ["/customer/{id}"], method = arrayOf(RequestMethod.GET))
+    fun getCustomer(@PathVariable id : Int) = customers[id]
 }

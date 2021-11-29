c1f5068bdee8795f6c1345991bf0cf326583cac8
seojinsarejj <s19009@naver.com>
2021-11-29 14:09:45 +0900

    Add getCustomers, getCustomer, createCustomer

--- ch3/src/main/kotlin/com/microservices/ch3/CustomerController.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerController.kt
@@ -9,10 +9,15 @@ class CustomerController {
     @Autowired
     lateinit var customers : ConcurrentHashMap<Int, Customer>
 
-    @RequestMapping(value = ["/customer"], method = arrayOf(RequestMethod.GET))
-    fun getCustomer(@RequestParam(required = false, defaultValue = "")
-    nameFilter: String) =
-        customers.filter {
-            it.value.name.contains(nameFilter, true)
-        }.map(Map.Entry<Int, Customer>::value).toList()
+    @RequestMapping(value = ["/customers"], method = arrayOf(RequestMethod.GET))
+    fun getCustomers() = customers.map(Map.Entry<Int, Customer>::value).toList()
+
+    @RequestMapping(value = ["/customer/{id}"],method = arrayOf(RequestMethod.GET))
+    fun getCustomer(@PathVariable id : Int) = customers[id]
+
+    @RequestMapping(value = ["/customer"], method = arrayOf(RequestMethod.POST))
+    fun createCustomer(@RequestBody customer: Customer) {
+        customers[customer.id] = customer
+    }
 }
+

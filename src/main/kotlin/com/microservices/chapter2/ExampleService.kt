6679ecad5196faf33001369ff8ee65d9392a1d55
seojinsarejj <s19009@naver.com>
2021-11-29 09:07:20 +0900

    Add value annotation on service

--- src/main/kotlin/com/microservices/chapter2/ExampleService.kt
+++ src/main/kotlin/com/microservices/chapter2/ExampleService.kt
@@ -1,8 +1,12 @@
 package com.microservices.chapter2
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class ExampleService : ServiceInterface {
-    override fun getHello(name : String) = "hello $name"
+    @Value(value = "\${service.message.text}")
+    private lateinit var text: String
+
+    override fun getHello(name : String) = "$text $name"
 }

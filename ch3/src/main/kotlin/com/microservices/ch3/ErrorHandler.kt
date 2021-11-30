ed26b93687a27a6b2a3588af631228c2931aa010
seojinsarejj <s19009@naver.com>
2021-11-30 16:07:55 +0900

    Create Error Handler

---
+++ ch3/src/main/kotlin/com/microservices/ch3/ErrorHandler.kt
@@ -0,0 +1,17 @@
+package com.microservices.ch3
+
+import com.fasterxml.jackson.core.JsonParseException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.servlet.http.HttpServletRequest
+
+@ControllerAdvice
+class ErrorHandler {
+    @ExceptionHandler(JsonParseException::class)
+    fun JsonParseExceptionHandler(servletRequest: HttpServletRequest,
+                                    exception: Exception): ResponseEntity<String> {
+        return ResponseEntity("JSON Error", HttpStatus.BAD_REQUEST)
+    }
+}

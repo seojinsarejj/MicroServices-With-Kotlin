f4e97f8df6ff237cc9a52ee7d4f820b09195613d
seojinsarejj <s19009@naver.com>
2021-11-30 09:05:18 +0900

    Delete customer bean on main application

--- ch3/src/main/kotlin/com/microservices/ch3/Ch3Application.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/Ch3Application.kt
@@ -6,16 +6,7 @@ import org.springframework.context.annotation.Bean
 import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class Ch3Application {
-	companion object {
-		val initialCutomers = arrayOf(Customer(1, "Kotlin"),
-			Customer(2,"Spring"),
-			Customer(3,"Microservice"))
-	}
-
-	@Bean
-	fun customer() = ConcurrentHashMap<Int,Customer>(initialCutomers.associateBy(Customer::id))
-}
+class Ch3Application
 
 fun main(args: Array<String>) {
 	runApplication<Ch3Application>(*args)

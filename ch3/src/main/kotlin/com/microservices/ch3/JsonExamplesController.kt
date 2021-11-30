4b08267feec868368582992d8019427acebd91ab
seojinsarejj <s19009@naver.com>
2021-11-30 14:58:58 +0900

    Update simple object to complex object

--- ch3/src/main/kotlin/com/microservices/ch3/JsonExamplesController.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/JsonExamplesController.kt
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class JsonExamplesController {
     @GetMapping(value = ["/json"])
-    fun getJson() = SimpleObject("hi","kotlin")
+    fun getJson() = ComplexObject(object1 = SimpleObject("more", "complex"))
 }

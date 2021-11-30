07b3c8762a5e489f860bd20aeb7211517c560d41
seojinsarejj <s19009@naver.com>
2021-11-30 15:08:29 +0900

    Add optional option to telephone object

--- ch3/src/main/kotlin/com/microservices/ch3/Customer.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/Customer.kt
@@ -1,5 +1,5 @@
 package com.microservices.ch3
 
-data class Customer (var id: Int = 0, var name: String = "", var telephone: Telephone) {
+data class Customer (var id: Int = 0, var name: String = "", var telephone: Telephone? = null) {
     data class Telephone(var countryCode: String = "", var telephoneNumber: String = "")
 }

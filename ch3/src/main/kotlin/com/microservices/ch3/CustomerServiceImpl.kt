e0373b6a379bc070fcb7b416a41d58ffc975e68e
seojinsarejj <s19009@naver.com>
2021-11-30 09:00:01 +0900

    Add customers object

--- ch3/src/main/kotlin/com/microservices/ch3/CustomerServiceImpl.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerServiceImpl.kt
@@ -1,6 +1,15 @@
 package com.microservices.ch3
 
+import java.util.concurrent.ConcurrentHashMap
+
 class CustomerServiceImpl : CustomerService {
+    companion object {
+        val initialCustomers = arrayOf(Customer(1,"Kotlin"),
+            Customer(2, "Spring"),
+            Customer(3, "Microservice"))
+    }
+    val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+
     override fun getCustomer(id: Int): Customer? {
         TODO("Not yet implemented")
     }

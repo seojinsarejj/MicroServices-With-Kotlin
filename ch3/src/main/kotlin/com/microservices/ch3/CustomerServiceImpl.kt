0eddd85b25cba03dd94fe281e46934ac6a2aece5
seojinsarejj <s19009@naver.com>
2021-11-30 15:05:03 +0900

    Add telephone object to customer object

--- ch3/src/main/kotlin/com/microservices/ch3/CustomerServiceImpl.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/CustomerServiceImpl.kt
@@ -1,14 +1,17 @@
 package com.microservices.ch3
 
+import com.microservices.ch3.Customer.Telephone
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class CustomerServiceImpl : CustomerService {
     companion object {
-        val initialCustomers = arrayOf(Customer(1,"Kotlin"),
-            Customer(2, "Spring"),
-            Customer(3, "Microservice"))
+        val initialCustomers = arrayOf(Customer(1,"Kotlin", Telephone("+44",
+            "7123456789")
+        ),
+            Customer(2, "Spring", Telephone("+44", "7123456789")),
+            Customer(3, "Microservice", Telephone("+44","7123456789")))
     }
     val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
 

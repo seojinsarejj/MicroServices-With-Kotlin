2d3ededdc002a352a9a7e42d9f17eba1af6bc806
seojinsarejj <s19009@naver.com>
2021-11-30 15:03:05 +0900

    Add telephone class to Customer object

--- ch3/src/main/kotlin/com/microservices/ch3/Customer.kt
+++ ch3/src/main/kotlin/com/microservices/ch3/Customer.kt
@@ -1,3 +1,5 @@
 package com.microservices.ch3
 
-data class Customer (var id: Int = 0, var name: String = "")
+data class Customer (var id: Int = 0, var name: String = "", var telephone: Telephone) {
+    data class Telephone(var countryCode: String = "", var telephoneNumber: String = "")
+}

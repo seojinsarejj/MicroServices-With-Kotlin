d5a6f33d4245e93bd25abcf7428c87e0ca10f9a2
seojinsarejj <s19009@naver.com>
2021-11-30 11:59:54 +0900

    Create Json example controller

---
+++ ch3/src/main/kotlin/com/microservices/ch3/JsonExamplesController.kt
@@ -0,0 +1,10 @@
+package com.microservices.ch3
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JsonExamplesController {
+    @GetMapping(value = ["/json"])
+    fun getJson() = SimpleObject()
+}

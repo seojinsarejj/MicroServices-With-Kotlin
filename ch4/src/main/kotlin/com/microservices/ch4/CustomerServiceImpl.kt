b0ec8da56446ec815543eecfc8216526cbb73273
seojinsarejj <s19009@naver.com>
2021-12-01 11:38:39 +0900

    Create CustomerServiceImpl.kt

---
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerServiceImpl.kt
@@ -0,0 +1,23 @@
+package com.microservices.ch4
+
+import com.microservices.ch4.Customer.Telephone
+import org.springframework.stereotype.Component
+import java.util.concurrent.ConcurrentHashMap
+
+@Component
+class CustomerServiceImpl : CustomerService {
+    companion object {
+        val initialCustomers = arrayOf(Customer(1,"Kotlin"),
+        Customer(2,"Spring"),
+        Customer(3,"Microservice", Telephone("+44","7123456789")))
+    }
+    val customers = ConcurrentHashMap<Int,Customer>(initialCustomers.associateBy(Customer::id))
+
+    override fun getCustomer(id: Int) = customers[id]
+
+    override fun searchCustomer(nameFilter: String): List<Customer> =
+        customers.filter {
+            it.value.name.contains(nameFilter, true)
+        }.map(Map.Entry<Int, Customer>::value).toList()
+
+}

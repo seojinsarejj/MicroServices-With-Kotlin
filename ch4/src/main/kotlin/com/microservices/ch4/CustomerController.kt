f72ae0fdd9ddbb0f208b4d137f799daa9b163e8f
seojinsarejj <s19009@naver.com>
2021-12-01 11:30:43 +0900

    Add response entity to get customer controller

--- ch4/src/main/kotlin/com/microservices/ch4/CustomerController.kt
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerController.kt
@@ -1,5 +1,7 @@
 package com.microservices.ch4
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -7,5 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class CustomerController {
     @GetMapping(value = ["/customer/{id}"])
-    fun getCustomer(@PathVariable id : Int) = Customer(id, "customer $id")
+    fun getCustomer(@PathVariable id : Int): ResponseEntity<Customer> {
+        return ResponseEntity(Customer(id, "customer $id"), HttpStatus.OK)
+    }
 }

4231447fc7a15cba995ba5c834aaeddcf5481e4f
seojinsarejj <s19009@naver.com>
2021-12-02 09:55:11 +0900

    Update searchCustomer on service return type to Flux

--- ch4/src/main/kotlin/com/microservices/ch4/CustomerService.kt
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerService.kt
@@ -1,8 +1,9 @@
 package com.microservices.ch4
 
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface CustomerService {
     fun getCustomer(id : Int) : Mono<Customer>?
-    fun searchCustomer(nameFilter: String) : List<Customer>
+    fun searchCustomer(nameFilter: String) : Flux<Customer>
 }

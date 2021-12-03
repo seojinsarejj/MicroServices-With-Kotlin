379848e473b8affc62d2fdc414f27e49f6ade9e5
seojinsarejj <s19009@naver.com>
2021-12-03 16:05:34 +0900

    Delete Unnecessary type and static import

--- ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
@@ -2,19 +2,18 @@ package com.microservices.ch4
 
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.toMono
 
 @Component
 class CustomerRouter {
     @Bean
-    fun customerRoutes(): RouterFunction<*> = router {
+    fun customerRoutes() = router {
         "/functional".nest {
             "/customer".nest{
                 GET("/") {
-                    ServerResponse.ok().body("hello world".toMono(),
+                    ok().body("hello world".toMono(),
                     String::class.java)
                 }
             }

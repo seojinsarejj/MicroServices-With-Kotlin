7cac4fde6c7d1f8b76d9d9ca12b0bcbd8665bd77
seojinsarejj <s19009@naver.com>
2021-12-03 16:07:04 +0900

    Update return value on customer routes

--- ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.toMono
+import reactor.kotlin.core.publisher.toMono
 
 @Component
 class CustomerRouter {
@@ -13,8 +14,7 @@ class CustomerRouter {
         "/functional".nest {
             "/customer".nest{
                 GET("/") {
-                    ok().body("hello world".toMono(),
-                    String::class.java)
+                    ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
                 }
             }
         }

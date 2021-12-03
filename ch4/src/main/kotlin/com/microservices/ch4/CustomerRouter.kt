7c2952b79d508358b8345e8c93f48409259d6bfd
seojinsarejj <s19009@naver.com>
2021-12-03 16:03:49 +0900

    Update customer router function

--- ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
+++ ch4/src/main/kotlin/com/microservices/ch4/CustomerRouter.kt
@@ -3,13 +3,21 @@ package com.microservices.ch4
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.toMono
 
 @Component
 class CustomerRouter {
     @Bean
     fun customerRoutes(): RouterFunction<*> = router {
         "/functional".nest {
+            "/customer".nest{
+                GET("/") {
+                    ServerResponse.ok().body("hello world".toMono(),
+                    String::class.java)
+                }
+            }
         }
     }
 }
